9c50ffe9f99e2c49b0ebc5c875c1bb87814a8fee
Brad Boyle <ojalaquellueva@gmail.com>
2020-09-28 14:36:33 -0700

    Revise user_data tables, add meta

--- cds_db/sql/create_core_tables.sql
+++ cds_db/sql/create_core_tables.sql
@@ -2,14 +2,18 @@
 -- Creates all remaining tables not derived from geonames
 -- -----------------------------------------------------------------
 
+DROP TABLE IF EXISTS meta;
+CREATE TABLE meta (
+db_version text DEFAULT NULL,
+code_version text DEFAULT NULL,
+build_date timestamp
+);
+
 DROP TABLE IF EXISTS user_data_raw;
 CREATE TABLE user_data_raw (
-user_id text DEFAULT NULL,
-country text DEFAULT NULL,
-state_province text DEFAULT NULL,
-county_parish text DEFAULT NULL,
-latitude numeric DEFAULT NULL,
-longitude numeric DEFAULT NULL
+latitude text DEFAULT NULL,
+longitude text DEFAULT NULL,
+user_id text DEFAULT NULL
 );
 
 
@@ -17,43 +21,35 @@ DROP TABLE IF EXISTS user_data;
 CREATE TABLE user_data (
 id BIGSERIAL NOT NULL PRIMARY KEY,
 job text DEFAULT NULL,
-poldiv_full text DEFAULT NULL,
-country VARCHAR(250) DEFAULT NULL,
-state_province VARCHAR(250) DEFAULT NULL,
-county_parish VARCHAR(250) DEFAULT NULL,
-country_iso VARCHAR(50) DEFAULT NULL,
-state_province_iso VARCHAR(50) DEFAULT NULL,
-county_parish_iso VARCHAR(50) DEFAULT NULL,
+batch integer DEFAULT NULL,
+latitude_verbatim text DEFAULT NULL,
+longitude_verbatim text DEFAULT NULL,
 latitude numeric DEFAULT NULL,
 longitude numeric DEFAULT NULL,
-user_id text DEFAULT NULL
-) 
-;
-
-DROP TABLE IF EXISTS cache;
-CREATE TABLE cache (
-id BIGSERIAL NOT NULL PRIMARY KEY,
-poldiv_full text DEFAULT NULL,
-country_verbatim text DEFAULT '',
-state_province_verbatim text DEFAULT '',
-county_parish_verbatim text DEFAULT '',
-country VARCHAR(250) DEFAULT NULL,
-state_province VARCHAR(250) DEFAULT NULL,
-county_parish VARCHAR(250) DEFAULT NULL,
-country_id INTEGER DEFAULT NULL,
-state_province_id INTEGER DEFAULT NULL,
-county_parish_id INTEGER DEFAULT NULL,
-country_iso VARCHAR(50) DEFAULT NULL,
-state_province_iso VARCHAR(50) DEFAULT NULL,
-county_parish_iso VARCHAR(50) DEFAULT NULL,
-match_method_country VARCHAR(50) DEFAULT NULL,
-match_method_state_province VARCHAR(50) DEFAULT NULL,
-match_method_county_parish VARCHAR(50) DEFAULT NULL,
-match_score_country NUMERIC(4,2) DEFAULT NULL,
-match_score_state_province NUMERIC(4,2) DEFAULT NULL,
-match_score_county_parish NUMERIC(4,2) DEFAULT NULL,
-poldiv_submitted VARCHAR(50) DEFAULT NULL, 
-poldiv_matched VARCHAR(50) DEFAULT NULL,
-match_status VARCHAR(50) DEFAULT NULL
+user_id text DEFAULT NULL,
+gid_0 text DEFAULT NULL,
+country text DEFAULT NULL,
+gid_1 text DEFAULT NULL,
+state text DEFAULT NULL,
+gid_2 text DEFAULT NULL,
+county text DEFAULT NULL,
+country_cent_dist numeric DEFAULT NULL,
+country_cent_dist_relative numeric DEFAULT NULL,
+country_cent_type text DEFAULT NULL,
+country_max_uncertainty numeric DEFAULT NULL,
+state_cent_dist numeric DEFAULT NULL,
+state_cent_dist_relative numeric DEFAULT NULL,
+state_cent_type text DEFAULT NULL,
+state_max_uncertainty numeric DEFAULT NULL,
+county_cent_dist numeric DEFAULT NULL,
+county_cent_dist_relative numeric DEFAULT NULL,
+county_cent_type text DEFAULT NULL,
+county_max_uncertainty numeric DEFAULT NULL,
+is_centroid smallint DEFAULT NULL,
+centroid_dist_relative numeric DEFAULT NULL,
+centroid_poldiv text DEFAULT NULL,
+centroid_type text DEFAULT NULL,
+centroid_max_uncertainty numeric DEFAULT NULL,
+latlong_err text DEFAULT NULL
 ) 
 ;
