58570b9c67804eff80a2694f7a45b6326350f2e5
Brad Boyle <ojalaquellueva@gmail.com>
2024-08-15 17:26:05 -0700

    First prod release (db 1.1, code 2.1.1)

---
+++ db/hotfix/2.1.1/update_meta.sql
@@ -0,0 +1,31 @@
+-- -----------------------------------------------------------------
+-- Update app metadata & increment version
+-- -----------------------------------------------------------------
+
+\c gvs_dev	-- Private development
+-- \c gvs		-- Public development and production
+
+
+-- Insert metadata for current version
+INSERT INTO META (
+db_version,
+db_version_comments,
+db_build_date,
+code_version,
+code_version_comments,
+code_release_date,
+citation,
+publication,
+logo_path
+)
+VALUES (
+'1.1',
+'Add metadata tables and data dictionary (dd_output)',
+'2024-06-10',
+'2.1.1',
+'First production release.',
+'2024-07-02',
+'@misc{gvs, author = {Boyle, B. L. and Maitner, B. and Park, D. S. and Rethvick, S. Y. B. and Enquist, B. J.}, journal = {Botanical Information and Ecology Network}, title = {Geocoordinate Validation Service}, year = 2024, url = {https://gvs.biendata.org/}} ',
+NULL,
+NULL
+);
